eb331f886bae965fcb1110aa779a997fdbd9c611
mlameira1 <157396233+mlameira1@users.noreply.github.com>
2024-01-22 17:32:03 +0100

    Update mssecret.py

--- mssecret.py
+++ mssecret.py
@@ -12,3 +12,6 @@ print("Fake Secret:", fake_secret)
 print("Fake Secret:", fake_secret)
 print("Fake Secret:", fake_secret)
 print("Fake Secret:", fake_secret)
+
+# Fake API Secret
+api_secret = "your_fake_api_secret_here"
